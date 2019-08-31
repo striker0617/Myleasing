a5bce514bbdb30bc91780c056a8502aaaa2c7a3e
OCTAVIO LOPEZ RIOS <octaviolopez56223@correo.itm.edu.co>
2019-08-31 09:26:46 -0500

    video 13 min 5

--- Myleasing.Web/Helpers/CombosHelper.cs
+++ Myleasing.Web/Helpers/CombosHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Myleasing.Web.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,30 @@ namespace Myleasing.Web.Helpers
 {
     public class CombosHelper
     {
+        private readonly DataContext _dataContext;
+
+        public CombosHelper(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
         public IEnumerable<SelectListItem> GetComboPropertyType()
         {
-            throw new NotImplementedException();
+            var List = _dataContext.PropertyTypes.Select(pt => new SelectListItem
+            {
+                Text  = pt.Name,
+                Value = $"{ pt.Id}"
+
+            })
+                .OrderBy(pt => pt.Text)
+                .ToList();
+
+            List.Insert(0, new SelectListItem
+            {
+                Text="(Select a property type...)",
+                Value="0"
+            });
+            return List;
+
         }
     }
 }
